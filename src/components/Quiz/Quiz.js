007490ef1f47ebc4f3d9531acb02188516246d98
NabilHassan7 <fakrulibnehassan@gmail.com>
2022-10-12 21:43:37 +0600

    completed quiz question component

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import QuizQuestion from '../QuizQuestion/QuizQuestion';
 
 const Quiz = () => {
     const quiz = useLoaderData().data;
     const quizQuestions = useLoaderData().data.questions;
+    console.log(quiz);
     console.log(quizQuestions);
-    // console.log(quizQuestions);
     return (
-        <div>
-            <div>{quiz.name}</div>
+        <div className='mt-20'>
+            <div className=' mb-10 text-blue-600 font-bold text-4xl'>
+                Quiz of {quiz.name}
+            </div>
+            <div className='flex flex-col gap-2'>
+                {
+                    quizQuestions.map(quizQuestion => <QuizQuestion
+                        id = {quizQuestion.id}
+                        quizQuestion = {quizQuestion}
+                    ></QuizQuestion>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/components/QuizQuestion/QuizQuestion.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { EyeIcon } from '@heroicons/react/24/solid'
+
+const QuizQuestion = ({quizQuestion}) => {
+    const {question, options, correctAnswer} = quizQuestion;
+    const checkAnswer = (event, answer) => {
+        if(answer === correctAnswer){
+            console.log("true");
+        }
+        else{
+            console.log('false');
+        }
+    }
+    return (
+        <div className='flex flex-col mt-10 border border-red-500 p-4 mx-10'>
+            <div className='border border-green-400 p-4'>
+                <h1 className=' font-bold text-2xl'>
+                    {question}
+                </h1>
+            </div>
+            <div className='flex justify-center items-center p-2'>
+                <div>
+                    <EyeIcon className="h-6 w-6 text-blue-500"/>
+                </div>
+            </div>
+            <div>
+                <div className='flex flex-col'>
+                    <div onClick={event => checkAnswer(event, options[0])} class=" w-full flex justify-center items-center mb-4 border border-green-400 p-2">
+                        <input id="default-radio-1" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 
+                        dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+                        <label for="default-radio-1" class="ml-2 text-lg font-semibold text-gray-900 dark:text-gray-300">{options[0]}</label>
+                    </div>
+                    <div onClick={event => checkAnswer(event, options[1])} class=" w-full flex justify-center items-center mb-4 border border-green-400 p-2">
+                        <input id="default-radio-1" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 
+                        dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+                        <label for="default-radio-1" class="ml-2 text-lg font-semibold text-gray-900 dark:text-gray-300">{options[1]}</label>
+                    </div>
+                    <div onClick={event => checkAnswer(event, options[2])} class=" w-full flex justify-center items-center mb-4 border border-green-400 p-2">
+                        <input id="default-radio-1" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 
+                        dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+                        <label for="default-radio-1" class="ml-2 text-lg font-semibold text-gray-900 dark:text-gray-300">{options[2]}</label>
+                    </div>
+                    <div onClick={event => checkAnswer(event, options[3])} class=" w-full flex justify-center items-center mb-4 border border-green-400 p-2">
+                        <input id="default-radio-1" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 
+                        dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+                        <label for="default-radio-1" class="ml-2 text-lg font-semibold text-gray-900 dark:text-gray-300">{options[3]}</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default QuizQuestion;
