a170695117f814f16ea864c16981ad3cb4e5b079
NabilHassan7 <fakrulibnehassan@gmail.com>
2022-10-12 22:26:03 +0600

    fixed minor issue in Topics Component

--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -5,7 +5,7 @@ import TopicCards from '../TopicCards/TopicCards';
 const Topics = () => {
     const topics = useLoaderData().data;
     return (
-        <div>
+        <div className='mt-20'>
            <div className='grid grid-cols-1 grid-rows-4 sm:grid-cols-4 sm:grid-rows-1 mx-10'>
                 {
                     topics.map(topic => <TopicCards
