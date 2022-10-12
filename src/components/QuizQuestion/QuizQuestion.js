1a44b222749e1088c5c03dd19ff1c060670202e0
NabilHassan7 <fakrulibnehassan@gmail.com>
2022-10-12 22:33:38 +0600

    fixed issue with answer id

--- src/components/QuizQuestion/QuizQuestion.js
+++ src/components/QuizQuestion/QuizQuestion.js
@@ -5,7 +5,8 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 const QuizQuestion = ({quizQuestion}) => {
-    const {question, options, correctAnswer} = quizQuestion;
+    const {question, options, correctAnswer, id} = quizQuestion;
+    console.log(quizQuestion);
     const checkAnswer = (event, answer) => {
         if(answer === correctAnswer){
             // console.log("true");
@@ -49,24 +50,24 @@ const QuizQuestion = ({quizQuestion}) => {
             <div>
                 <div className='flex flex-col'>
                     <div onClick={event => checkAnswer(event, options[0])} class=" w-full flex justify-center items-center mb-4 border border-green-400 p-2">
-                        <input id="default-radio-1" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 
+                        <input id={id} type="radio" value="" name={id} class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 
                         dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
-                        <label for="default-radio-1" class="ml-2 text-lg font-semibold text-gray-900 dark:text-gray-300">{options[0]}</label>
+                        <label for={id} class="ml-2 text-lg font-semibold text-gray-900 dark:text-gray-300">{options[0]}</label>
                     </div>
                     <div onClick={event => checkAnswer(event, options[1])} class=" w-full flex justify-center items-center mb-4 border border-green-400 p-2">
-                        <input id="default-radio-1" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 
+                        <input id={id} type="radio" value="" name={id} class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 
                         dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
-                        <label for="default-radio-1" class="ml-2 text-lg font-semibold text-gray-900 dark:text-gray-300">{options[1]}</label>
+                        <label for={id} class="ml-2 text-lg font-semibold text-gray-900 dark:text-gray-300">{options[1]}</label>
                     </div>
                     <div onClick={event => checkAnswer(event, options[2])} class=" w-full flex justify-center items-center mb-4 border border-green-400 p-2">
-                        <input id="default-radio-1" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 
+                        <input id={id} type="radio" value="" name={id} class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 
                         dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
-                        <label for="default-radio-1" class="ml-2 text-lg font-semibold text-gray-900 dark:text-gray-300">{options[2]}</label>
+                        <label for={id} class="ml-2 text-lg font-semibold text-gray-900 dark:text-gray-300">{options[2]}</label>
                     </div>
                     <div onClick={event => checkAnswer(event, options[3])} class=" w-full flex justify-center items-center mb-4 border border-green-400 p-2">
-                        <input id="default-radio-1" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 
+                        <input id={id} type="radio" value="" name={id} class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 
                         dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
-                        <label for="default-radio-1" class="ml-2 text-lg font-semibold text-gray-900 dark:text-gray-300">{options[3]}</label>
+                        <label for={id} class="ml-2 text-lg font-semibold text-gray-900 dark:text-gray-300">{options[3]}</label>
                     </div>
                 </div>
             </div>
