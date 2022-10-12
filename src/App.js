51cc7d39d9fd894faa8bae29a9cbe8012e4f6a24
NabilHassan7 <fakrulibnehassan@gmail.com>
2022-10-12 12:48:55 +0600

    setup createBrowserRouter and simple routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
+  // Creating the browser router
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Navbar></Navbar>
+    },
+    { path: '/home', element: <Navbar></Navbar>},
+    { path: '/*', element: <div>Error 404</div>}
+  ])
   return (
     <div className="App">
-      <Navbar></Navbar>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -33,7 +33,7 @@ const Navbar = () => {
                     }
                 </div>
             </div>
-            <div>
+            <div className=' text-left h-10'>
                 Hello
             </div>
         </nav>
